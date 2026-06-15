a016f988780f050acb1c4f91bb7704f8745166af
pedro271206-stack <pedro271206@gmail.com>
2026-06-15 06:14:05 -0300

    Delete Gerenciamento_de_Compras-CRUD-main directory

--- Gerenciamento_de_Compras-CRUD-main/src/db/seeds/exemplo_produtos.js
+++
@@ -1,9 +0,0 @@
-exports.seed = async function(knex) {
-  await knex('produtos').del();
-
-  await knex('produtos').insert([
-    { nome: 'Café', descricao: 'Cafeína pura!', preco: 100.00, data_inclusao: '2026-01-01 00:00:00' },
-    { nome: 'Arroz', descricao: 'Grão nutritivo!', preco: 200.00, data_inclusao: '2026-01-01 00:00:00' },
-    { nome: 'Feijão', descricao: 'Grão nutritivo!', preco: 300.00, data_inclusao: '2026-01-01 00:00:00' }
-  ]);
-};
